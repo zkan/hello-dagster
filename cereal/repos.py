1c6b33affaf284153feab33731c15bd7ff7ac055
zkan <kan@prontomarketing.com>
2020-02-28 16:21:51 +0700

    :sparkles: Include the rest of pipelines (but can't run since solid can't be redeclared) :bear:

--- cereal/repos.py
+++ cereal/repos.py
@@ -2,13 +2,17 @@ from dagster import RepositoryDefinition
 
 from complex_pipeline import complex_pipeline
 from hello_cereal import hello_cereal_pipeline
+from inputs import inputs_pipeline
+from serial_pipeline import serial_pipeline
 
 
 def define_repo():
     return RepositoryDefinition(
         name='hello_cereal_repository',
         pipeline_dict={
-            'hello_cereal_pipeline': lambda: hello_cereal_pipeline,
             'complex_pipeline': lambda: complex_pipeline,
+            'hello_cereal_pipeline': lambda: hello_cereal_pipeline,
+            # 'inputs_pipeline': lambda: inputs_pipeline,
+            # 'serial_pipeline': lambda: serial_pipeline,
         },
     )
