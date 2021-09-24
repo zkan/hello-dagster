67cd585a034c099fe83664633ec1bde572f99c3c
zkan <zkan.cs@gmail.com>
2021-09-24 12:24:49 +0700

    Change some name :bear:

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -7,18 +7,18 @@ from dagster import (
 
 @solid
 def get_name():
-    return "dagster"
+    return "Kan"
 
 
 @solid
-def hello(context, name: str):
-    context.log.info(f"Hello, {name}!")
+def hey(context, name: str):
+    context.log.info(f"Hey, {name}!")
 
 
 @pipeline
-def hello_pipeline():
-    hello(get_name())
+def hey_pipeline():
+    hey(get_name())
 
 
 if __name__ == "__main__":
-    result = execute_pipeline(hello_pipeline)
+    result = execute_pipeline(hey_pipeline)
